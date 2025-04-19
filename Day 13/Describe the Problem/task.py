13ce7e5b7d5c00e6c19a106e85192c1d1298eed2
Nick Dzitars <dzitarscomputing@gmail.com>
2025-04-19 01:59:04 -0400

    Completed day 13, debugging

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -1,5 +1,5 @@
 def my_function():
-    for i in range(1, 20):
+    for i in range(1, 21):
         if i == 20:
             print("You got it")
 
@@ -8,5 +8,8 @@ my_function()
 
 # Describe the Problem - Write your answers as comments:
 # 1. What is the for loop doing?
+# The for loop is running 19 times, looking to see if the number is 20 and when it is, it prints out "you got it"
 # 2. When is the function meant to print "You got it"?
+# When i = 20, the 20th iteration of the loop
 # 3. What are your assumptions about the value of i?
+# range() doesn't include the max value in the range, so it goes between 1-19
--- Day 13/Use Print/task.py
+++ Day 13/Use Print/task.py
@@ -1,5 +1,7 @@
 word_per_page = 0
 pages = int(input("Number of pages: "))
-word_per_page == int(input("Number of words per page: "))
+word_per_page = int(input("Number of words per page: "))
+print(f"Pages: {pages}")
+print(f"Words per page: {word_per_page}")
 total_words = pages * word_per_page
 print(total_words)
